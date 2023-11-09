a7cc7adbe3ed25f97014f97ddae2090508d385cf
scottA <scot.atkinson@live.co.uk>
2023-11-09 10:24:51 +0000

    balence changes

--- functions/battletower.py
+++ functions/battletower.py
@@ -1,7 +1,7 @@
 battleTower =[
     {'stage':1,"enemy":"agumon" ,"hp":3, "atk":1,"power":12,"prize":100,"xp":15},
-    {'stage':2,"enemy":"agumon(black)" ,"hp":3, "atk":1,"power":5,"prize":100,"xp":15},
-    {'stage':3,"enemy":"numemon" ,"hp":3, "atk":1,"power":5,"prize":100,"xp":20}
+    {'stage':2,"enemy":"agumon(black)" ,"hp":3, "atk":1,"power":15,"prize":100,"xp":15},
+    {'stage':3,"enemy":"numemon" ,"hp":5, "atk":2,"power":28,"prize":100,"xp":20}
 ]
 
 
