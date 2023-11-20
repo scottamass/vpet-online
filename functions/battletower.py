ba06ef855c44dd920a2cf26b3fe2235456875ad5
scottA <scot.atkinson@live.co.uk>
2023-11-20 14:27:19 +0000

    battle tower roster update

--- functions/battletower.py
+++ functions/battletower.py
@@ -3,7 +3,12 @@ battleTower =[
     {'stage':2,"enemy":"agumon(black)" ,"hp":3, "atk":1,"power":15,"prize":100,"xp":15},
     {'stage':3,"enemy":"numemon" ,"hp":5, "atk":2,"power":28,"prize":100,"xp":20},
     {'stage':4,"enemy":"darktyranomon" ,"hp":9, "atk":2,"power":35,"prize":150,"xp":25},
-    {'stage':5,"enemy":"MissingNo" ,"hp":20, "atk":5,"power":220,"prize":150,"xp":25},
+    {'stage':5,"enemy":"greymon" ,"hp":9, "atk":3,"power":35,"prize":150,"xp":25},
+    {'stage':6,"enemy":"monochromon" ,"hp":9, "atk":2,"power":35,"prize":150,"xp":25},
+    {'stage':7,"enemy":"metalgreymon(vacine)" ,"hp":12, "atk":3,"power":105,"prize":150,"xp":25},    
+    {'stage':8,"enemy":"darktyranomon" ,"hp":9, "atk":2,"power":35,"prize":150,"xp":25},
+    {'stage':9,"enemy":"darktyranomon" ,"hp":9, "atk":2,"power":35,"prize":150,"xp":25},    
+    {'stage':10,"enemy":"MissingNo" ,"hp":20, "atk":5,"power":220,"prize":150,"xp":25},
 
 ]
 
