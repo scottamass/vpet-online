e8b234a9026ad13e1e9b227a982d104f9245b735
scottA <scot.atkinson@live.co.uk>
2023-11-28 15:13:17 +0000

    new db job added

--- dbjobs.py
+++ dbjobs.py
@@ -9,10 +9,19 @@ db= MongoClient(mongo_connect)
 def dbjobsonerun():
     #db.monsterdb.monsters.update_many({},{'$set': {"seris": 1}})
     #db.playerMonster.monsters.update_many({},{'$set': {"seris": 1}})
-    db.playerMonster.monsters.update_many({},{'$set': {"wins": 0}})
-    db.playerMonster.monsters.update_many({},{'$set': {"losses": 0}})    
-    db.userProfiles.userProfiles.update_many({},{'$set': {"wins": 0}})
-    db.userProfiles.userProfiles.update_many({},{'$set': {"losses": 0}})
+    #db.playerMonster.monsters.update_many({},{'$set': {"wins": 0}})
+    #db.playerMonster.monsters.update_many({},{'$set': {"losses": 0}})    
+    #db.userProfiles.userProfiles.update_many({},{'$set': {"wins": 0}})
+    #db.userProfiles.userProfiles.update_many({},{'$set': {"losses": 0}})
+    for monsters in db.playerMonster.monsters.find({}):
+        print(monsters['monster_id'])
+        m = monsters['monster_id']
+        mid = monsters['_id']
+        monsterdb= db.monsterdb.monsters.find_one({'id':m})
+        print(monsterdb)
+        mtype=monsterdb['type']
+        db.playerMonster.monsters.update_one({"_id":mid},{'$set':{'type':mtype}})
+        #db.playerMonster.monster.update_one{}
 
 
     return 'ok'
