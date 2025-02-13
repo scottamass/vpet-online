878ed2a89e1c9fd83f75fd529b56b4598e15e064
scottA <scot.atkinson@live.co.uk>
2025-02-13 14:56:12 +0000

    added shop window

--- blueprints/gameplay.py
+++ blueprints/gameplay.py
@@ -1,4 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import current_user
 
-gameplay_bp = Blueprint('gameplay', __name__)
+from functions.dbfunct import evocheck, fetch_player_monster
 
+gameplay_bp = Blueprint('gameplay', __name__ ,template_folder='templates')
+
+#Screens 
+
+#main game loop
+@gameplay_bp.route('/app/monster') 
+def monster():
+    monster=fetch_player_monster(current_user.id)
+    if monster != None:
+        evocheck(current_user.id)
+        monster=fetch_player_monster(current_user.id)
+    backgroundUrl='/static/bg/1.jpeg'
+    return render_template('/app/partials/screens/gameSreen.html',monster=monster)
+
+@gameplay_bp.route('/app/shop')
+def shop():
+    return render_template('app/partials/screens/shop.html')
+    #return 'TEST'
